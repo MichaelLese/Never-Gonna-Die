acbb382aa8cf5282bfe126c4bc031474fa887522
Michael <mlese1@binghamton.edu>
2024-11-09 14:29:30 -0500

    basic movement down with inertia
    Added basic movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float smoothTime = 0.1f;
+
+    private Vector2 moveDirection;
+    private Vector2 currentVelocity = Vector2.zero;
+    private float moveAngle;
+
+    Vector3 dir;
+
+    public void updateMovement(Rigidbody2D rb) {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        // Lerp current velocity towards the target direction
+        currentVelocity = Vector2.Lerp(currentVelocity, moveDirection * moveSpeed, smoothTime * Time.deltaTime);
+        // Apply the lerped velocity to the Rigidbody
+        rb.velocity = currentVelocity;
+
+        moveAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg - 90f;
+
+        dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public Movement movement;
+    //public Dash dash;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Initialize Health
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.updateMovement(rb);
     }
 }
